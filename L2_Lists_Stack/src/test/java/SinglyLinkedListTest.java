aa9486bd9c6019ac92db4482bcf8f66181fe7692
Joshua Villing <jsvilling@gmail.com>
2020-02-27 10:15:08 +0100

    Add more unit tests for SinglyLinkedList
    L2

--- L2_Lists_Stack/src/test/java/SinglyLinkedListTest.java
+++ L2_Lists_Stack/src/test/java/SinglyLinkedListTest.java
@@ -58,62 +58,56 @@ public class SinglyLinkedListTest {
     @Test
     public void removeFirst() {
         // Given
+        list = new SinglyLinkedList<>();
+        list.insertFirst("2");
+        list.insertFirst("q");
+        list.insertAfter("4", 0);
+        list.insertAfter("5", 1);
 
         // When
+        list.removeFirst();
 
         // Then
+        Assert.assertEquals("5", list.get(0));
+        Assert.assertEquals("4", list.get(1));
+        Assert.assertEquals("q", list.get(2));
+        Assert.assertEquals(3, list.size());
     }
 
     @Test
     public void remove() {
         // Given
+        list = new SinglyLinkedList<>();
+        list.insertFirst("2");
+        list.insertFirst("q");
+        list.insertAfter("4", 0);
+        list.insertAfter("5", 1);
 
         // When
+        list.remove(2);
 
         // Then
+        Assert.assertEquals("5", list.get(0));
+        Assert.assertEquals("4", list.get(1));
+        Assert.assertEquals("2", list.get(2));
+        Assert.assertEquals(3, list.size());
     }
 
     @Test
     public void removeAll() {
         // Given
+        list = new SinglyLinkedList<>();
+        list.insertFirst("2");
+        list.insertFirst("q");
+        list.insertAfter("4", 0);
+        list.insertAfter("5", 1);
 
         // When
+        list.removeAll();
 
         // Then
-
+        Assert.assertEquals(0, list.size());
     }
-
-    @Test
-    public void getFirst() {
-        // Given
-
-        // When
-
-        // Then
-
-    }
-
-    @Test
-    public void get() {
-        // Given
-
-        // When
-
-        // Then
-
-    }
-
-
-    @Test
-    public void size() {
-        // Given
-
-        // When
-
-        // Then
-
-    }
-
 
     @Test
     public void testToString() {
