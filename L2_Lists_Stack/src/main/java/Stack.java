90a73093945d00c6bdac921c2d7fbcdc1d5e644d
Joshua Villing <jsvilling@gmail.com>
2020-02-28 10:45:08 +0100

    Implement Stack using SinglyLinkedList
    L2

---
+++ L2_Lists_Stack/src/main/java/Stack.java
@@ -0,0 +1,30 @@
+/**
+ * Stack Implementation using {@link SinglyLinkedList<T>} for ALGD2
+ *
+ * @author J. Villing
+ */
+public class Stack<T> {
+    private final SinglyLinkedList<T> list;
+
+    public Stack() {
+        list = new SinglyLinkedList<>();
+    }
+
+    public T top() {
+        return list.getFirst();
+    }
+
+    public void push(T data) {
+        list.insertFirst(data);
+    }
+
+    public T pop() {
+        T first = list.getFirst();
+        list.removeFirst();
+        return first;
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+}
---
+++ L2_Lists_Stack/src/test/java/StackTest.java
@@ -0,0 +1,55 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+
+public class StackTest {
+
+    @Test
+    public void testPushElement() {
+        // Given
+        Stack<String> stack = new Stack<>();
+
+        // When
+        stack.push("data");
+
+        // Then
+        Assert.assertEquals("data", stack.top());
+        Assert.assertFalse(stack.isEmpty());
+    }
+
+    @Test
+    public void testPopElement() {
+        // Given
+        Stack<String> stack = new Stack<>();
+        stack.push("data");
+
+        // When
+        String element = stack.pop();
+
+        // Then
+        Assert.assertEquals("data", element);
+        Assert.assertTrue(stack.isEmpty());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testPopOnEmptyStack() {
+        // Given
+        Stack<String> stack = new Stack<>();
+
+        // When
+        stack.pop();
+    }
+
+    @Test
+    public void testIsEmpty() {
+        // Given
+        Stack<String> stack = new Stack<>();
+
+        // Then
+        Assert.assertTrue(stack.isEmpty());
+    }
+
+}
