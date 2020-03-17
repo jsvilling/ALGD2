079b10d1b06ecbecd441b8538802102c6a427726
Joshua Villing <joshua.villing@students.fhnw.ch>
2020-03-17 11:22:05 +0100

    Add binary tree solution

---
+++ L4_Trees/src/main/java/ch/fhnw/algd2/tree/solution/main_binTree.java
@@ -0,0 +1,38 @@
+package ch.fhnw.algd2.tree.solution;//******************************************************************************
+//  FHNW.ALGD2  -  Excercise 4 : Testing Binary Search Trees                   *
+// --------------------------------------------------------------------------  *
+//  version 2                                                             vtg  * 
+//******************************************************************************
+
+public class main_binTree {
+
+  public static void main(String[] args) {
+    int[] values = {12, 19, 31, 37, 43, 49, 51, 55, 67, 70, 78, 81, 87, 92, 99};
+    BinaryTree bt = new BinaryTree(values);
+    //BinaryTree bt = new BinaryTree();
+    bt.show();
+    System.out.println("------------------------------------");
+
+//    bt.testInternalSearch(55);
+//    System.out.println("---------------------------");
+//    bt.testInternalSearch(37);
+//    System.out.println("---------------------------");
+//    bt.testInternalSearch(81);
+//    System.out.println("---------------------------");
+//    bt.testInternalSearch(0);
+
+    //bt.insert(13);
+    bt.insert(50);
+    bt.show();
+    System.out.println("------------------------------------");
+
+    //bt.remove(51);
+    //bt.remove(49);
+    bt.remove(55);
+    bt.remove(81);
+
+    bt.show();
+
+  }
+
+}
